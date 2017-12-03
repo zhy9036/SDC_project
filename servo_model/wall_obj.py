9011769075694147c5ea47208a5927e2231b9823
yang <yang.zhang7@wsu.edu>
2017-12-02 19:18:18 -0800

    code improvement

--- servo_model/wall_obj.py
+++ servo_model/wall_obj.py
@@ -18,9 +18,9 @@ class Action(Enum):
 class SDC_wall:
 	thread_list = []
 	pin_list = [21, 20, 16, 19, 13, 6]
-	edge_dict = {'a':{'pins':[21, 20], 'speeds': [76, 115, 115, 75], 'timers': [3.3, 3.8, 1.5, 6]},
-				 'b':{'pins':[16, 19], 'speeds': [77, 115, 115, 75], 'timers': [3.3, 3.3, 1.8, 6]},
-				 'c':{'pins':[13, 6], 'speeds': [77, 115, 115, 76], 'timers': [3.1, 3.3, 0.8, 6]},
+	edge_dict = {'a':{'pins':[21, 20], 'status': Action.CLOSE, 'speeds': [76, 115, 115, 75], 'timers': [3.3, 3.8, 1.5, 6]},
+				 'b':{'pins':[16, 19], 'status': Action.CLOSE, 'speeds': [77, 115, 115, 75], 'timers': [3.3, 3.3, 1.8, 6]},
+				 'c':{'pins':[13, 6], 'status': Action.CLOSE, 'speeds': [77, 115, 115, 76], 'timers': [3.1, 3.3, 0.8, 6]},
 				 }
 
 		
@@ -63,8 +63,11 @@ class SDC_wall:
 		open_spd = self.edge_dict[edge]['speeds'][:2]
 		close_timer = self.edge_dict[edge]['timers'][2:]
 		close_spd = self.edge_dict[edge]['speeds'][2:]
-		
+		status = self.edge_dict[edge]['status']
 		if action == Action.OPEN:
+			if status == Action.OPEN:
+				print('Try to open Edge %s, but edge %s already opened' % (edge, edge), end='\r')
+				return
 			#t1 = _thread.start_new_thread(self.move_servo, (pins[0], open_spd[0], open_timer[0]))
 			#t2 = _thread.start_new_thread(self.move_servo, (pins[1], open_spd[1], open_timer[1]))
 			t1 = threading.Thread(target=self.move_servo, args=(pins[0], open_spd[0], open_timer[0]))
@@ -73,14 +76,19 @@ class SDC_wall:
 			t2.start()
 			self.thread_list.append(t1)
 			self.thread_list.append(t2)
+			self.edge_dict[edge]['status'] = Action.OPEN
 		
 		elif action == Action.CLOSE:
+			if status == Action.CLOSE:
+				print('Try to close Edge %s, but edge %s already closed' % (edge, edge), end='\r')
+				return
 			t1 = threading.Thread(target=self.move_servo, args=(pins[0], close_spd[0], close_timer[0]))
 			t2 = threading.Thread(target=self.move_servo, args=(pins[1], close_spd[1], close_timer[1]))
 			t1.start()
 			t2.start()
 			self.thread_list.append(t1)
 			self.thread_list.append(t2)
+			self.edge_dict[edge]['status'] = Action.CLOSE
 			
 	def is_running(self):
 		self.thread_list = [t for t in self.thread_list if t.isAlive()]
